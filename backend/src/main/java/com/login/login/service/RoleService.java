9c285ce7a7a100f6b588d1dd3406353f7d101a7b
LimWaiLeongJeremy <jereremy19995@hotmail.sg>
2023-10-20 21:33:37 +0800

    added Role service

---
+++ backend/src/main/java/com/login/login/service/RoleService.java
@@ -0,0 +1,18 @@
+package com.login.login.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.login.login.model.Role;
+import com.login.login.repository.RoleRepository;
+
+@Service
+public class RoleService {
+
+    @Autowired
+    private RoleRepository roleRepo;
+
+    public Role createNewRole(Role role) {
+        return roleRepo.save(role);
+    }
+}
