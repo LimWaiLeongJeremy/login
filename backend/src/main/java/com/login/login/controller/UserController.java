52a63a91570c00adee209b83c272e5d903b5492a
LimWaiLeongJeremy <jereremy19995@hotmail.sg>
2023-10-21 13:03:25 +0800

    added user controller

---
+++ backend/src/main/java/com/login/login/controller/UserController.java
@@ -0,0 +1,24 @@
+package com.login.login.controller;
+
+import javax.annotation.PostConstruct;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.login.login.service.UserService;
+
+@Controller
+@RequestMapping(path = "/api")
+@CrossOrigin
+public class UserController {
+    
+    @Autowired
+    private UserService userSrc;
+    
+    @PostConstruct
+    public void initRolesAndUser() {
+        userSrc.initRolesAndUser();
+    }
+}
