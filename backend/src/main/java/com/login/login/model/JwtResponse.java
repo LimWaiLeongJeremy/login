cf1b3ce8b84c1302f0a9f129521f146d7f81f918
LimWaiLeongJeremy <jereremy19995@hotmail.sg>
2023-10-21 15:06:18 +0800

    created JWT response model

---
+++ backend/src/main/java/com/login/login/model/JwtResponse.java
@@ -0,0 +1,20 @@
+package com.login.login.model;
+
+public class JwtResponse {
+    
+    private User user;
+    private String jwtToken;
+    
+    public User getUser() {
+        return user;
+    }
+    public void setUser(User user) {
+        this.user = user;
+    }
+    public String getJwtToken() {
+        return jwtToken;
+    }
+    public void setJwtToken(String jwtToken) {
+        this.jwtToken = jwtToken;
+    } 
+}
