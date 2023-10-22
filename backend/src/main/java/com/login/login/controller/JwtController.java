8f73b43444c08abc1497a23593f870630558fca0
LimWaiLeongJeremy <jereremy19995@hotmail.sg>
2023-10-22 19:00:15 +0800

    added JWT controller class

---
+++ backend/src/main/java/com/login/login/controller/JwtController.java
@@ -0,0 +1,24 @@
+package com.login.login.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.login.login.model.JwtRequest;
+import com.login.login.model.JwtResponse;
+import com.login.login.service.JwtService;
+
+@RestController
+@RequestMapping(path = "/api")
+public class JwtController {
+    
+    @Autowired
+    private JwtService jwtSrc;
+
+    @PostMapping({ "/authenticate" })
+    public JwtResponse createJwtToken(@RequestBody JwtRequest jwtReq) throws Exception {
+        return jwtSrc.createJwtToken(jwtReq);
+    }
+}
