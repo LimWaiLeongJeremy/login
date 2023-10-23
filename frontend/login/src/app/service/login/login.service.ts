162e1cfb5860a61f37f816575908e30e682b5efd
LimWaiLeongJeremy <jereremy19995@hotmail.sg>
2023-10-23 23:29:14 +0800

    added logic for the login src

--- frontend/login/src/app/service/login/login.service.ts
+++ frontend/login/src/app/service/login/login.service.ts
@@ -1,10 +1,52 @@
 import { Injectable } from '@angular/core';
 import { environment } from "../environment";
+import { HttpClient, HttpHeaders } from "@angular/common/http";
+import { AuthService } from "../auth/auth.service";
+import { AuthResponse } from "../../model/authResponse";
+import { Roles } from 'src/app/model/role';
+
 
 @Injectable({
   providedIn: 'root'
 })
 export class LoginService {
 
-  constructor() { }
+  endpoint = environment.serverUrl;
+  requestHeader = new HttpHeaders({ 'No-Auth': 'true' });
+  userName!: string;
+  password!: string;
+  body = {};
+  token = this.authSrc.getToken() || '';
+
+  constructor(private authSrc: AuthService, private http: HttpClient) { }
+
+  public login(loginData: any) {
+    this.userName = loginData.userName;
+    this.password = loginData.password;
+    return this.http.post<AuthResponse>(
+      this.endpoint + '?authenticate',
+      loginData,
+      {
+        headers: this.requestHeader
+      }
+    );
+  }
+
+  public roleMatc(allowedRoles: string | any[]): boolean {
+    let isMatch = false;
+    const userRoles: Roles[] = this.authSrc.getRole();
+    if (userRoles != null && userRoles) {
+      for (let i = 0; i < userRoles.length; i++) {
+        for (let j = 0; j < allowedRoles.length; j++) {
+          if (userRoles[i].role === allowedRoles[j]) {
+            isMatch = true;
+            return isMatch;
+          } else {
+            return isMatch;
+          }
+        }
+      }
+    }
+    return isMatch;
+  }
 }
