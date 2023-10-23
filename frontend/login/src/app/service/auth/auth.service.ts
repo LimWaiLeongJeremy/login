14a7f3bdfe0ccda82067bd1547e5013116c98d7b
LimWaiLeongJeremy <jereremy19995@hotmail.sg>
2023-10-23 23:27:58 +0800

    added logic to the auth service

--- frontend/login/src/app/service/auth/auth.service.ts
+++ frontend/login/src/app/service/auth/auth.service.ts
@@ -1,9 +1,35 @@
 import { Injectable } from '@angular/core';
+import { Subject } from "rxjs";
+import { Roles } from "../../model/role";
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthService {
+
+
+  private token!: string;
+  private tokenSubject = new Subject<string>();
+  private usernameSubject = new Subject<string>();
+  token$ = this.tokenSubject.asObservable();
+  userName$ = this.usernameSubject.asObservable();
+  private roleChange = new Subject<any>();
+
+  public getRole() {
+    let role;
+    try {
+      const roleString = sessionStorage.getItem('roles') || '{}';
+      role = JSON.parse(roleString);
+    } catch (error) {
+      console.info(error);
+    }
+    return role;
+  }
+
+  public getToken() {
+    return sessionStorage.getItem('jwtToken');
+  }
+
 
   constructor() { }
 }
