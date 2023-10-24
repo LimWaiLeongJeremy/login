0d03bd784636415482f6a9a1372c8add146088e7
LimWaiLeongJeremy <jereremy19995@hotmail.sg>
2023-10-24 14:12:55 +0800

    init login form

--- frontend/login/src/app/component/login-page/login-page.component.ts
+++ frontend/login/src/app/component/login-page/login-page.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-login-page',
@@ -6,5 +7,21 @@ import { Component } from '@angular/core';
   styleUrls: ['./login-page.component.css']
 })
 export class LoginPageComponent {
+  form!: FormGroup;
+  constructor(
+    private fb: FormBuilder,
+  ) { }
+
+  ngOnInit(): void {
+    this.form = this.createCredential();
+  }
+
+  
+  private createCredential(): FormGroup {
+    return this.fb.group({
+      username: this.fb.control<string>('', Validators.required),
+      password: this.fb.control<string>('', Validators.required),
+    });
+  }
 
 }
