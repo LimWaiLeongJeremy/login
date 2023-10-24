6293f66900bd9c2c3719dca8f0be82b5855abb88
LimWaiLeongJeremy <jereremy19995@hotmail.sg>
2023-10-24 14:26:07 +0800

    added routing

--- frontend/login/src/app/app.module.ts
+++ frontend/login/src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { NgModule, isDevMode } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { ReactiveFormsModule } from "@angular/forms";
+import { Routes, RouterModule } from "@angular/router";
 
 import { AppComponent } from './app.component';
 import { ServiceWorkerModule } from '@angular/service-worker';
@@ -8,6 +9,23 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { LoginPageComponent } from './component/login-page/login-page.component';
 import { HomePageComponent } from './component/home-page/home-page.component';
 import { AdminPageComponent } from './component/admin-page/admin-page.component';
+
+import { InputTextModule } from 'primeng/inputtext';
+import { ButtonModule } from 'primeng/button';
+
+
+const routes: Routes = [
+  {
+    path: '',
+    title: 'Login page',
+    component: LoginPageComponent,
+  },
+  {
+    path: 'login',
+    title: 'Login page',
+    component: LoginPageComponent,
+  }
+]
 
 @NgModule({
   declarations: [
@@ -25,7 +43,10 @@ import { AdminPageComponent } from './component/admin-page/admin-page.component'
       registrationStrategy: 'registerWhenStable:30000'
     }),
     BrowserAnimationsModule,
-    ReactiveFormsModule
+    ReactiveFormsModule,
+    RouterModule.forRoot(routes),
+    ButtonModule,
+    InputTextModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
