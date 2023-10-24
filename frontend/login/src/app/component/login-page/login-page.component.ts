7e61eecfef0e9f233b8b9a584aca5fe0e8bf76cf
LimWaiLeongJeremy <jereremy19995@hotmail.sg>
2023-10-24 17:16:27 +0800

    created login page and it's logic

--- frontend/login/src/app/component/login-page/login-page.component.ts
+++ frontend/login/src/app/component/login-page/login-page.component.ts
@@ -1,5 +1,12 @@
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { LoginService } from "src/app/service/login/login.service";
+import { AuthService } from "src/app/service/auth/auth.service";
+import { Credential } from "src/app/model/credential";
+import { AuthResponse } from 'src/app/model/authResponse';
+import { Subscription } from 'rxjs';
+import { Router } from "@angular/router";
+
 
 @Component({
   selector: 'app-login-page',
@@ -8,18 +15,41 @@ import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 })
 export class LoginPageComponent {
   form!: FormGroup;
+  credential!: Credential;
+  sub!: Subscription;
   constructor(
     private fb: FormBuilder,
+    private loginSrc: LoginService,
+    private authSrc: AuthService,
+    private router: Router,
   ) { }
 
   ngOnInit(): void {
     this.form = this.createCredential();
   }
 
+
+  async submit() {
+    this.credential = this.form.value as Credential;
+    this.sub = this.loginSrc.login(this.credential).subscribe(
+      (response: AuthResponse) => {
+        sessionStorage.clear();
+        localStorage.clear;
+        this.authSrc.setUserName(response.user.userName);
+        this.authSrc.setRole(response.user.role);
+        this.authSrc.setToken(response.jwtToken);
+        localStorage.setItem('token', response.jwtToken);
+        this.router.navigateByUrl('/home');
+      },
+      (error) => {
+        console.info(error);
+      }
+    )
+  }
   
   private createCredential(): FormGroup {
     return this.fb.group({
-      username: this.fb.control<string>('', Validators.required),
+      userName: this.fb.control<string>('', Validators.required),
       password: this.fb.control<string>('', Validators.required),
     });
   }
